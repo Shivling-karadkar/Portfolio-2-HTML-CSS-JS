b77a57fdd654283deaca5f1bba6292f3e2fbb4a7
Shivling-karadkar <shivlingkaradkar@gmail.com>
2025-04-04 15:15:38 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+new Typed('.multiple-text', {
+    strings: [ 'Devops Engineer', 'Frontend Developer','Backend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
